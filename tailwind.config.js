6b20e8ca03f4ef26afc064f570b51dc388e5ef87
Jorge Casaudoumecq <Jorge@Casaudoumecq.me>
2023-07-19 10:36:17 -0400

    added needed images

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'deep-black': '#151515',
+      },
+    },
   },
   plugins: [],
 }
